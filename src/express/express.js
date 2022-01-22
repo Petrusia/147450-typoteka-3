df3aed34d0f7cacb25ab3d07b400116e248d22ba
petras <petrasblazevicius1@gmail.com>
2022-01-22 22:05:11 +0200

    3.15.1 Шаблонизируй это

--- src/express/express.js
+++ src/express/express.js
@@ -15,11 +15,12 @@ app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-app.use(`/articles`, articlesRoutes);
-app.use(`/my`, myRoutes);
+
 app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
 
 app.use((req, res) => res.status(404).render(`errors/404`));
-app.use((err, req, res, _next) => res.status(404).render(`errors/500`));
+app.use((err, req, res, _next) => res.status(500).render(`errors/500`));
 
 app.listen(DEFAULT_PORT);
